d774a67bb619934ab3573193ee1c4849f52431ab
lmjanssens <s1110698@student.hsleiden.nl>
2019-04-11 16:00:17 +0200

    Initial (not yet functioning) push

--- src/main/java/nl/hsleiden/controller/CustomerController.java
+++ src/main/java/nl/hsleiden/controller/CustomerController.java
@@ -1,14 +1,13 @@
 package nl.hsleiden.controller;
 
+import nl.hsleiden.exception.ResourceNotFoundException;
 import nl.hsleiden.model.Customer;
 import nl.hsleiden.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -27,5 +26,26 @@ public class CustomerController {
         return customerRepository.save(customer);
     }
 
-    
+    @PutMapping("/customers/{customerId}")
+    public Customer updateCustomer(@PathVariable Long customerId, @Valid @RequestBody Customer updatedCustomer) {
+        return customerRepository.findById(customerId).map(customer -> {
+            customer.setAddress(updatedCustomer.getAddress());
+            customer.setCity(updatedCustomer.getCity());
+            customer.setCountry(updatedCustomer.getCountry());
+            customer.setFirstName(updatedCustomer.getFirstName());
+            customer.setInfix(updatedCustomer.getInfix());
+            customer.setLastName(updatedCustomer.getLastName());
+            customer.setTitle(updatedCustomer.getTitle());
+            customer.setZipcode(updatedCustomer.getZipcode());
+            return customerRepository.save(customer);
+        }).orElseThrow(() -> new ResourceNotFoundException("Customer not found with id " + customerId));
+    }
+
+    @DeleteMapping("/customers/{customerId}")
+    public ResponseEntity<?> deleteCustomer(@PathVariable Long customerId) {
+        return customerRepository.findById(customerId).map(customer -> {
+            customerRepository.delete(customer);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ResourceNotFoundException("Customer not found with id" + customerId));
+    }
 }
