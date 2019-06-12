b57e7c5041a744700711f605fed0a5645955495d
Danny van Tol <danny.van.tol@outlook.com>
2019-06-12 13:37:56 +0200

    Send user id back to the client

--- src/main/java/nl/hsleiden/model/User.java
+++ src/main/java/nl/hsleiden/model/User.java
@@ -16,6 +16,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "userid", columnDefinition = "serial")
+    @JsonView(View.Public.class)
     private Long id;
 
     @NotNull
