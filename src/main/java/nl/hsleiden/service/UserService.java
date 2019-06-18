5f84676d4833a334cba89b724385d2dbddccc8d7
Danny van Tol <danny.van.tol@outlook.com>
2019-06-18 13:55:56 +0200

    Add UserService to the git repository

---
+++ src/main/java/nl/hsleiden/service/UserService.java
@@ -0,0 +1,31 @@
+package nl.hsleiden.service;
+
+import nl.hsleiden.model.User;
+import nl.hsleiden.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import sun.security.util.Password;
+
+@Component
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public User encodePassword(User user) {
+        user.setPassword(this.passwordEncoder().encode(user.getPassword()));
+        return user;
+    }
+
+    public User updatePassword(User user) {
+        return userRepository.save(
+                this.encodePassword(user)
+        );
+    }
+
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
