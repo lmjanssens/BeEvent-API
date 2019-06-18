d74c2bd95b6fd11c1e2b68496b2db2ae7ff3d99c
Robin Silvério <robinsilverio17@gmail.com>
2019-06-18 14:56:44 +0200

    Partially created imageupload feature. Ready to be revised by Danny

--- src/main/java/nl/hsleiden/Application.java
+++ src/main/java/nl/hsleiden/Application.java
@@ -1,5 +1,7 @@
 package nl.hsleiden;
 
+import nl.hsleiden.service.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,8 +9,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import javax.annotation.Resource;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+    @Resource
+    private StorageService storageService;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -20,5 +28,11 @@ public class Application {
                 registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET","POST","PUT","DELETE");
             }
         };
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        storageService.deleteAll();
+        storageService.init();
     }
 }
